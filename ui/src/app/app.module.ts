f84beee45300ee33428704cd69357eccb25f6e6d
hydrogen2oxygen <luxuspeter@googlemail.com>
2026-01-29 20:35:20 +0100

    Proof of concept works

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import {HashLocationStrategy, LocationStrategy} from "@angular/common";
-import {HTTP_INTERCEPTORS} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {LoadingInterceptor} from "./services/loadingInterceptor";
 import { StatisticsComponent } from './components/statistics/statistics.component';
 
@@ -17,7 +17,8 @@ import { StatisticsComponent } from './components/statistics/statistics.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [
     {provide: LocationStrategy, useClass: HashLocationStrategy},
--- ui/src/app/components/read/read.component.ts
+++ ui/src/app/components/read/read.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {OllamaService} from "../../services/ollama.service";
+import {FormControl, ReactiveFormsModule} from "@angular/forms";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-read',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    NgIf
+  ],
   templateUrl: './read.component.html',
   styleUrl: './read.component.scss'
 })
-export class ReadComponent {
+export class ReadComponent implements OnInit {
+  importText = new FormControl('', { nonNullable: true })
+  text = new FormControl('', { nonNullable: true })
+  responseText = ""
+  loading: boolean = false;
 
+  constructor(private ollama:OllamaService ) {}
+
+  ngOnInit(): void {
+    this.importText.setValue("Утром я готовлю завтрак.\n" +
+      "Я беру яйца и готовлю яичницу.\n" +
+      "Яйца жарятся быстро и вкусно пахнут.\n" +
+      "Потом я делаю кофе.\n" +
+      "Кофе горячий и ароматный.\n" +
+      "Я сажусь за стол и ем завтрак.\n" +
+      "Завтрак с яйцами и кофе очень вкусный.\n" +
+      "Так приятно начинать утро с хорошего завтрака.")
+  }
+
+
+  saveText() {
+    console.log("saveText: " + this.importText.value);
+    this.text.setValue(this.importText.value);
+  }
+
+  testOllama() {
+    this.loading = true;
+
+    let question = `Take from this text the most important words and return it as a comma-separated list (only the most important words and avoid comments and other stuff): \n\n${this.text.value}`;
+
+    this.ollama.chat(question).subscribe(response => {
+      console.log("Ollama response: ", response);
+      this.responseText = response;
+      this.loading = false;
+    });
+  }
 }
